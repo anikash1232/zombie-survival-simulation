14b3288a523c79652cca4286c5077f9f317d9b1d
anikash1232 <anirudh.vpk@gmail.com>
2025-10-26 19:18:06 -0400

    first push

--- src/main/java/com/comp301/a07zombie/Base.java
+++ src/main/java/com/comp301/a07zombie/Base.java
@@ -1,14 +1,51 @@
 package com.comp301.a07zombie;
 
-public class Base implements IBase {
-  @Override
-  public void addSupplies(int amount) {}
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-  @Override
-  public int getSupplyCount() {
-    return 0;
+public class Base implements IBase {
+  private int SupplyCount;
+  private Lock lock;
+
+  public Base(){
+    this.SupplyCount = 0;
+    lock = new ReentrantLock();
   }
 
   @Override
-  public void useTool(String task) {}
+  public synchronized void addSupplies(int amount) {
+    if (amount <= 0){
+      throw new IllegalArgumentException();
+    }
+    else{
+      SupplyCount += amount;
+      System.out.println(Thread.currentThread().getName() + "added" + amount + "supplies. Total: " + this.SupplyCount);
+    }
+
+  }
+
+  @Override
+  public synchronized int getSupplyCount() {
+    return this.SupplyCount;
+  }
+
+  @Override
+  public void useTool(String task) {
+    lock.lock();
+    try{
+      System.out.println("Survivor is using tool for" + task);
+      Thread.sleep(1000);
+      System.out.println("Survivor is no longer using tool!");
+    }
+    catch(InterruptedException e){
+      System.out.println("oops sum went wrong!");
+    }
+    finally{
+      lock.unlock();
+    }
+
+
+
+
+  }
 }
--- src/main/java/com/comp301/a07zombie/Survivor.java
+++ src/main/java/com/comp301/a07zombie/Survivor.java
@@ -1,20 +1,62 @@
 package com.comp301.a07zombie;
 
+import java.util.Random;
+
 public class Survivor implements ISurvivor {
+    private Base base;
+    private static boolean flag = false;
 
-  public Survivor(Base base) {}
+    public Survivor(Base base) {
+        this.base = base;
+    }
 
-  @Override
-  public void stop() {}
+    @Override
+    public void stop() {
+        flag = true;
+    }
 
-  @Override
-  public void run() {}
+    @Override
+    public void run() {
+        while (!flag) {
+            try {
+                performAction();
+            } catch (InterruptedException e) {
+                stop();
+            }
+        }
+    }
 
-  protected void performAction() throws InterruptedException {}
+    protected void performAction() throws InterruptedException {
+        Random random = new Random();
+        int zeroThruTwo = random.nextInt(3);
 
-  protected void scavenge() throws InterruptedException {}
+        if (zeroThruTwo == 0) {
+            rest();
+        } else if (zeroThruTwo == 1) {
+            scavenge();
+        } else if (zeroThruTwo == 2) {
+            fortify();
+        }
+    }
 
-  protected void fortify() throws InterruptedException {}
+    protected void scavenge() throws InterruptedException {
+        Random random = new Random();
+        int min = 1000;
+        int max = 4000;
+        int number = random.nextInt(max - min + 1) + min;
+        System.out.println("Survivor is scavenging for supplies!");
+        Thread.sleep(number);
+        base.addSupplies(2);
+    }
 
-  protected void rest() throws InterruptedException {}
+    protected void fortify() throws InterruptedException {
+        base.useTool("fortification");
+        System.out.println("Survivor is fortifying the base!");
+        Thread.sleep(2000);
+    }
+
+    protected void rest() throws InterruptedException {
+        System.out.println("Survivor is resting!");
+        Thread.sleep(2000);
+    }
 }
